79b35e007234e02e212d5e0a9e1f709c009bb9ca
abdelmalek mazouzi <aforar.business@gmail.com>
2025-06-12 02:52:33 +0100

    start to create widget

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import *
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    root = tkinter.Tk()
+    root = Label(root, text='GeeksForGeeks.org!')
+    root.pack()
+
+    root.mainloop()
